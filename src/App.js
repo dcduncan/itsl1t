aa948ec5814c1b2c3cce07b5f7083a238dd94546
Daniel Duncan <dduncan@medallia.com>
2018-08-09 09:36:05 -0700

    Attempts to hit the backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    id: 0,
+    content: 'World!',
+  };
+
+  setName = (event) => {
+    console.log(event);
+    axios.get(`https://itsl1t-rest.herokuapp.com/greeting?name=${event.target.value}`)
+      .then(result => {
+        this.setState({
+          ...result
+        });
+      })
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +30,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type='text' value={this.state.name} onChange={this.setName} />
+        <p>{this.state.name}:{this.state.count}</p>
       </div>
     );
   }
