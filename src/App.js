0f51ec000f538b1bd3de6e1cd94c9cbd0d679772
Daniel Duncan <dduncan@medallia.com>
2018-08-16 08:46:53 -0700

    temp commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ImageUploader from 'react-images-upload';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,9 @@ class App extends Component {
   state = {
     count: 0,
     content: 'World!',
+    pictures: null,
+    urls: null,
+    selectedFile: null
   };
 
   setName = (event) => {
@@ -19,7 +23,69 @@ class App extends Component {
           count: result.data.id,
           content: result.data.content
         });
+      }).catch((err) => {
+        console.log('Failed to set name');
+        console.log(err);
       })
+  };
+
+  //
+  // onDrop = (pictures, urls) => {
+  //   this.setState({
+  //     pictures,
+  //     urls
+  //   });
+  // };
+
+  fileChangedHandler = (event) => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+
+  uploadPictures = () => {
+    // if (this.state.pictures.length === 0) {
+    //   return;
+    // }
+    //
+    // const formData = new FormData();
+    // for (let i = 0; i < this.state.pictures.length; ++i) {
+    //   formData.append(`${i}`, this.state.pictures[i], this.state.urls[i]);
+    // }
+    //
+    // axios.post(
+    //   'https://itsl1t-rest.herokuapp.com/upload/images',
+    //   formData, {
+    //     headers: {
+    //       'Access-Control-Allow-Origin': 'POST'
+    //     }
+    //   })
+    //   .then(result => {
+    //   console.log(`upload success, result: ${result}`);
+    // })
+    //   .catch((err) => {
+    //   console.log(`Failed to upload`);
+    //   console.log(err);
+    // });
+
+    if (!this.state.selectedFile) {
+      return;
+    }
+
+    axios.post(
+      'https://itsl1t-rest.herokuapp.com/upload/images',
+      this.state.selectedFile, {
+        headers: {
+          'Access-Control-Allow-Origin': 'POST'
+        }
+      })
+      .then(result => {
+      console.log(`upload success, result: ${result}`);
+    })
+      .catch((err) => {
+      console.log(`Failed to upload`);
+      console.log(err);
+    });
   };
 
   render() {
@@ -34,6 +100,18 @@ class App extends Component {
         </p>
         <input type='text' value={this.state.content} onChange={this.setName} />
         <p>{this.state.content}:{this.state.count}</p>
+        {/*<div style={{ width: '33%', height: '300px', display: 'inline-block' }}>*/}
+          {/*<ImageUploader*/}
+            {/*withIcon={true}*/}
+            {/*withPreview={true}*/}
+            {/*buttonText='Choose image'*/}
+            {/*onChange={this.onDrop}*/}
+            {/*imgExtension={['.jpg', '.png']}*/}
+            {/*maxFileSize={5242880}*/}
+          {/*/>*/}
+        {/*</div>*/}
+        <input type="file" onChange={this.fileChangedHandler} />
+        <input type="button" value="Upload Pictures" onClick={this.uploadPictures} />
       </div>
     );
   }
